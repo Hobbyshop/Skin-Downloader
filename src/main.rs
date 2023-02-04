c7c3f01419273f1a1352f48ed1921300c45911ec
Hobbyshop <marc.deike@outlook.de>
2023-02-04 17:33:38 +0100

    implemented error handling

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@ use regex::Regex;
 fn main() {
     println!("Please enter a valid username, its skin will be downloaded");
 
+    let username = get_username_from_io().unwrap();
+    println!("{}", username);
+}
+
+fn get_username_from_io() -> Result<String, &'static str> {
     let mut input = String::new();
     print!("> ");
 
     let _ = stdout().flush();
     stdin().read_line(&mut input).expect("Invalid string input!");
 
-    let ex = Regex::new(r"^[a-zA-Z0-9_]{3,16}$").unwrap();
-    println!("{}", ex.is_match(&input));
+    let ex = Regex::new(r"^[a-zA-Z-1-9_]{3,16}$").unwrap();
+
+    if ex.is_match(&input.trim()) {
+       Ok(input)
+    } else {
+        Err("Invalid username format!")
+    }
 }

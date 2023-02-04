906d19cba4bdb3aa015c1149d88315e58818abc0
Hobbyshop <marc.deike@outlook.de>
2023-02-04 17:15:05 +0100

    validate usernames

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{stdout, Write, stdin};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello World");
+    println!("Please enter a valid username, its skin will be downloaded");
+
+    let mut input = String::new();
+    print!("> ");
+
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).expect("Invalid string input!");
+
+    let ex = Regex::new(r"^[a-zA-Z0-9_]{3,16}$").unwrap();
+    println!("{}", ex.is_match(&input));
 }

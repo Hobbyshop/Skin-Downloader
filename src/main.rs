0a344bf88e1d79aa65169de5ee94ee38ecf7b55f
Hobbyshop <marc.deike@outlook.de>
2023-02-04 21:24:27 +0100

    added log info

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,32 @@ async fn main() -> Result<(), reqwest::Error> {
 
     let username = get_username_from_io().unwrap();
 
-    let user_data = http::get_as_json("https://api.mojang.com/users/profiles/minecraft/".to_owned() + username.as_str()).await?;
+    logger::log(&String::from(
+        "Requesting user uuid from \"https://api.mojang.com/users/profiles/minecraft/".to_owned()
+        + username.as_str()
+        + "\""
+    ));
+
+    let user_data = http::get_as_json("https://api.mojang.com/users/profiles/minecraft/".to_owned() + username.as_str()).await
+        .expect(&String::from("Could not find user with name \"".to_owned() + &username + "\""));
+    
     let username = &user_data["name"];
 
+    logger::log(&String::from(
+        "Found user: \"".to_owned()
+        + &username
+        + "\" with uuid \""
+        + user_data["id"].as_str()
+        + "\""
+    ));
+
     let skin_bytes = http::get_as_bytes("https://crafatar.com/skins/".to_owned() + &user_data["id"]).await?;
+
+    logger::log(&String::from(
+        "Downloaded skin from \"https://crafatar.com/skins/".to_owned()
+        + &user_data["id"]
+        + "\""
+    ));
 
     let file = File::create(username.to_owned() + ".png").expect("Could not find nor create the image file!");
     write_to_file(file, skin_bytes);
@@ -42,4 +64,5 @@ fn get_username_from_io() -> Result<String, &'static str> {
 
 fn write_to_file(mut file: File, bytes: Bytes) {
     file.write(&bytes).unwrap();
+    logger::log("Image saved");
 }
